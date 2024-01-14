ae4c6aa289a2c85cdef6b610c8a5296518fad6b4
SuperMK15 <smartiskush@gmail.com>
2024-01-14 03:03:22 -0500

    oops

--- testmaker-backend/config/allowedOrigins.js
+++ testmaker-backend/config/allowedOrigins.js
@@ -1,5 +1,5 @@
 const allowedOrigins = [
-    process.env.NODE_ENV === 'production' ? 'hhttps://studai-8en7.onrender.com' : 'http://localhost:5173'
+    process.env.NODE_ENV === 'production' ? 'https://studai-8en7.onrender.com' : 'http://localhost:5173'
 ];
 
 module.exports = allowedOrigins;
